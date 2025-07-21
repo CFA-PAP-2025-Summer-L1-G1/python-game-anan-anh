af6ebb8fe965a434b00c376ec32001dc95efc1ff
Anan Jemal <ananjemal@gmail.com>
2025-07-21 14:24:59 -0700

    Initial commit

---
+++ my_game.py
@@ -0,0 +1,19 @@
+from game_funcs import *
+import game_engine
+
+#Set this first to decide the world dimensions
+set_game_size(1600, 900)
+#Below is where most of your code will be written
+
+
+
+
+
+# WARNING: For advanced students/game requirements
+# Called once per frame (there are 60 frames per second)
+# DO NOT CHANGE FUNCTION NAME
+def update():
+    pass
+
+#DO NOT EDIT BELOW 
+game_engine.start(update)
