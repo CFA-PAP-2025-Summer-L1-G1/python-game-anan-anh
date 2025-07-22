e0b10b89ad70bb56bc39e60fc2493735e0a595f3
Anan J <ananjemal@gmail.comJ>
2025-07-22 15:44:52 -0700

    testing branch push

--- my_game.py
+++ my_game.py
@@ -5,9 +5,13 @@ import game_engine
 set_game_size(1600, 900)
 #Below is where most of your code will be written
 
+#title code
+title = print_text("Welcome to Challenger! Click on monsters to defeat them", 60)
+place_element(title, 300, 30)
 
-print('test')
-
+#background code
+background_color("black")
+add_background('game_background.png') 
 
 # WARNING: For advanced students/game requirements
 # Called once per frame (there are 60 frames per second)
