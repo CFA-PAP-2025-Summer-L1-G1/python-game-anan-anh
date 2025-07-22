e6941e454c4abc8947224d7c451c9fd2e80b3de9
Anan J <ananjemal@gmail.comJ>
2025-07-22 16:39:12 -0700

    Added player and monster

--- my_game.py
+++ my_game.py
@@ -13,6 +13,13 @@ place_element(title, 300, 30)
 background_color("black")
 add_background('game_background.png') 
 
+#player and monsters
+player = add_image("player.png", 300)
+place_element(player, 60, 500)
+
+monster1 = add_image("monster1.png", 500)
+place_element(monster1, 800, 350)
+
 # WARNING: For advanced students/game requirements
 # Called once per frame (there are 60 frames per second)
 # DO NOT CHANGE FUNCTION NAME
